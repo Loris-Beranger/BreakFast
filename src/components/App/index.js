441c1e46fdd7dc45e21e59fef966e7fc354771f9
karenbomer <karenmaciel@outlook.fr>
2022-04-04 13:27:17 +0200

    integration statique des elements principaux

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@
 import './styles.scss';
 import { Routes, Route, Link } from 'react-router-dom';
 import HomePage from 'src/components/HomePage';
+import BakeryList from 'src/components/BakeryList';
 import CheckoutBasket from 'src/components/CheckoutBasket';
 import reactDom from 'react-dom';
 
@@ -11,6 +12,7 @@ const App = () => (
   <div className="app">
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/bakerylist" element={<BakeryList />} />
       <Route path="/yi" element={<CheckoutBasket />} />
     </Routes>
     
---
+++ src/components/BakeryList/index.js
@@ -0,0 +1,35 @@
+// == Import
+import './style.scss';
+
+import image from './images/coffee-img.jpg'
+
+const BakeryList = () => {
+
+  return (
+    <div>
+      <div className="bakery">
+        <img className="bakery__img" src={image} alt="photo cafe" />
+        <p className="bakery-deliverytime">30-10 min</p>
+        <h2 className="bakery-name">O'croissant</h2>
+        <p className="bakery-deliverycost">Frais de livraison estimé : 2.50€</p>
+        <div className="rating">
+          <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+        </div>
+      </div>
+
+      <div className="bakery">
+        <img className="bakery__img" src={image} alt="photo cafe" />
+        <p className="bakery-deliverytime">30-10 min</p>
+        <h2 className="bakery-name">O'croissant</h2>
+        <p className="bakery-deliverycost">Frais de livraison estimé : 2.50€</p>
+        <div className="rating">
+          <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+// == Export
+export default BakeryList;
