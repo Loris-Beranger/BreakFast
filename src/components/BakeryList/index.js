df5032d41271c50fe03765ca09e61ddf61076810
karenbomer <karenmaciel@outlook.fr>
2022-04-05 11:18:11 +0200

    star rating ok

--- src/components/BakeryList/index.js
+++ src/components/BakeryList/index.js
@@ -1,20 +1,21 @@
 // == Import
-import './style.scss';
+import './styles.scss';
 
-import StarRating from 'src/components/StarRating';
+import StarRating from 'src/components/StarRating/StarRating';
 
 import image from './images/coffee-img.jpg';
 
 const BakeryList = () => {
- 
+
   return (
     <div>
+
       <div className="bakery">
         <img className="bakery__img" src={image} alt="photo cafe" />
         <p className="bakery-deliverytime">30-10 min</p>
         <h2 className="bakery-name">O'croissant</h2>
         <p className="bakery-deliverycost">Frais de livraison estimé : 2.50€</p>
-        <div className="rating">
+        <div>
           <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
         </div>
       </div>
@@ -22,10 +23,16 @@ const BakeryList = () => {
       <div className="bakery">
         <img className="bakery__img" src={image} alt="photo cafe" />
         <p className="bakery-deliverytime">30-10 min</p>
-        <h2 className="bakery-name">O'croissant</h2>
-        <p className="bakery-deliverycost">Frais de livraison estimé : 2.50€</p>
+
+        <div className="bakery-infos">
+          <h2 className="bakery-name">O'croissant</h2>
+          <p className="bakery-deliverycost">Frais de livraison estimé : 2.50€</p>
+        </div>
+        <div className="rating">
+          <StarRating />
+        </div>
       </div>
-      <StarRating />
+
     </div>
 
   );
--- src/components/StarRating/StarRating.js
+++ src/components/StarRating/StarRating.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
+import './styles.scss';
+
 const StarRating = () => {
   const [rating, setRating] = useState(null);
 
   return (
     <div>
       {[...Array(5)].map((star, i) => {
-        const ratingValue = i + 1;
+        const ratingValue = i++;
 
         return (
           <label>
@@ -17,7 +19,7 @@ const StarRating = () => {
               value={ratingValue}
               onClick={() => setRating(ratingValue)}
             />
-            <FaStar className="star" color={ratingValue < rating ? "#ffc107" : "#000000"} />
+            <FaStar className="star" color={ratingValue <= rating ? "#ffc107" : "#000000"} />
           </label>
         );
       })}
