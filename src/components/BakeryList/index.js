0d4d66c165a41b04c7070d367569ced09e5e4865
karenbomer <karenmaciel@outlook.fr>
2022-04-06 12:12:35 +0200

    responsive version pc en cours

--- src/components/BakeryList/index.js
+++ src/components/BakeryList/index.js
@@ -2,7 +2,7 @@
 import './styles.scss';
 
 import StarRating from 'src/components/StarRating/StarRating';
-import Tags from 'src/components/Tags/Tags'
+//import Tags from 'src/components/Tags/Tags'
 
 import croissant from './images/croissant.jpg';
 import pain from './images/pain.jpg';
@@ -11,53 +11,50 @@ import cupcake from './images/cupcake.jpg';
 const BakeryList = () => {
 
   return (
-    <div className="bakeries">
-      <Tags />
 
-      <div>
+        <div className="bakeries">
 
-        <div className="bakery">
-          <img className="bakery__img" src={croissant} alt="croissant" />
-          <p className="bakery-deliverytime">10-20 min</p>
+          <div className="bakery">
+            <img className="bakery__img" src={croissant} alt="croissant" />
+            <p className="bakery-deliverytime">10-20 min</p>
 
-          <div className="bakery-infos">
-            <h2 className="bakery-name">O'croissant</h2>
-            <p className="bakery-deliverycost">Frais de livraison estimé : 1.50€</p>
+            <div className="bakery-infos">
+              <h2 className="bakery-name">O'croissant</h2>
+              <p className="bakery-deliverycost">Frais de livraison estimé : 1.50€</p>
+            </div>
+            <div className="rating">
+              <StarRating />
+            </div>
           </div>
-          <div className="rating">
-            <StarRating />
+
+          <div className="bakery">
+            <img className="bakery__img" src={pain} alt="pain" />
+            <p className="bakery-deliverytime">20-30 min</p>
+
+            <div className="bakery-infos">
+              <h2 className="bakery-name">Seigle et Son</h2>
+              <p className="bakery-deliverycost">Frais de livraison estimé : 2.50€</p>
+            </div>
+            <div className="rating">
+              <StarRating />
+            </div>
           </div>
+
+          <div className="bakery">
+            <img className="bakery__img" src={cupcake} alt="cupcake" />
+            <p className="bakery-deliverytime">30-40 min</p>
+
+            <div className="bakery-infos">
+              <h2 className="bakery-name">Les délices de Karen</h2>
+              <p className="bakery-deliverycost">Frais de livraison estimé : 3.50€</p>
+            </div>
+            <div className="rating">
+              <StarRating />
+            </div>
+          </div>
+
         </div>
-
-        <div className="bakery">
-          <img className="bakery__img" src={pain} alt="pain" />
-          <p className="bakery-deliverytime">20-30 min</p>
-
-          <div className="bakery-infos">
-            <h2 className="bakery-name">Seigle et Son</h2>
-            <p className="bakery-deliverycost">Frais de livraison estimé : 2.50€</p>
-          </div>
-          <div className="rating">
-            <StarRating />
-          </div>
-        </div>
-
-        <div className="bakery">
-          <img className="bakery__img" src={cupcake} alt="cupcake" />
-          <p className="bakery-deliverytime">30-40 min</p>
-
-          <div className="bakery-infos">
-            <h2 className="bakery-name">Les délices de Karen</h2>
-            <p className="bakery-deliverycost">Frais de livraison estimé : 3.50€</p>
-          </div>
-          <div className="rating">
-            <StarRating />
-          </div>
-        </div>
-
-      </div>
-    </div>
-
+   
   );
 }
 
