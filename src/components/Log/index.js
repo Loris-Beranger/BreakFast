09aca88aa34a16a061e184122bf856a8101184ff
MathysVitiello <mathys.vitiello@gmail.com>
2022-04-07 15:10:14 +0200

    page login/connexion static done

--- src/components/Log/index.js
+++ src/components/Log/index.js
@@ -1,10 +1,48 @@
 // == Import
 import './styles.scss';
+import Field from 'src/components/Field';
 
 // == Composant
 const Log = () => (
   <div className="log">
-    <h1>Log</h1>
+    <div class="parent">
+      <div class="connexion">
+        Connexion
+          <div className='mail' >
+            Adresse e-mail
+              <div className='champ' >
+                <input className="input"/>
+              </div>
+          </div>
+          <div className='mdp' >
+            Mot de passe
+            <div className='champ' >
+              <input className="input"/>
+            </div>
+          </div>
+        </div>
+      <div class="inscription">
+        Inscription
+        <div className='mail' >
+          Adresse e-mail
+          <div className='champ' >
+            <input className="input"/>
+          </div>
+        </div>
+        <div className='mdp' >
+          Mot de passe
+          <div className='champ' >
+            <input className="input"/>
+          </div>
+        </div>
+        <div className='mdp' >
+          Confirmation de mot de passe 
+          <div className='champ' >
+            <input className="input"/>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
