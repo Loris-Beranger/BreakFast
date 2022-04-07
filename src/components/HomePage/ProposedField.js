16a8eb176a2e321dcc6e5917f3dbf997463851f8
Loris-Beranger <lolo.beranger[C1@gmail.com>
2022-04-07 12:21:20 +0200

    proposition champs adresse

---
+++ src/components/HomePage/ProposedField.js
@@ -0,0 +1,12 @@
+// == Import
+import './styles.scss';
+
+// == Composant
+const ProposedField = ({ name }) => (
+  <li className="proposedfield">
+    {name}
+  </li>
+);
+
+// == Export
+export default ProposedField;
