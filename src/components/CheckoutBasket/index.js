0ffe2b760126364cbffd3843615685d2ca6a5550
MathysVitiello <mathys.vitiello@gmail.com>
2022-04-08 15:24:47 +0200

    incrementaiton reréparé

--- src/components/CheckoutBasket/index.js
+++ src/components/CheckoutBasket/index.js
@@ -10,67 +10,50 @@ import React, { useState } from 'react';
 // == Composant
 const CheckoutBasket = () => {
 
-  
-
-let ProductNumber = 8;
-let blocProduct = [];
-let blocProductPrice = [];
-let Totalàpayer = 30;
-// totalàpayer devrait etre une addition de tout les prix; suremnt plus simple avec l'API 100%
-let price = 16
-  
-  
-// pour chaque produit (different) on boucle l'etiquette; et les lignes de prix
-  for (let i = 0; i<ProductNumber; i++){
-
     const [count, setCount] = useState(0);
-    if (count <= -1){
-      count = 0
-    }
-    
-    blocProduct.push(
-      <div className='Basket-product'>
-        <img className='Basket-imgProduit' src={imageCroissant} alt="img-croissant" />
-        <div className='Basket-quantity'>{count}
-            <div className='math'>
-              <img onClick={() => setCount(count - 1)} className='less' src={less} alt="signe moins" />
-              <img onClick={() => setCount(count + 1)} className='plus' src={plus} alt="signe plus" />
-            </div>
-        </div>     
-      </div>
-    )};
-    
-    for (let i = 0; i<ProductNumber; i++){
-
-      blocProductPrice.push(
-        <div className='Basket-ligne'>
-          <div className='Basket-productName'>petit croissant au beurre</div>
-          <div className='Basket-productPrice'>{price}</div>
-        </div>
-      )};
-
+    if(count < 0){
+      setCount(0)
+    }  
 
   return (
     <div className='Basket-Basket'>
       <div className='Basket-products'>
-        {blocProduct}
+
+        <div className='Basket-product'>
+          <img className='Basket-imgProduit' src={imageCroissant} alt="img-croissant" />
+          <div className='Basket-boite' >
+            <div className='Basket-product-name' >croissant</div>
+              <div className='Basket-quantity'>{count}
+                <div className='math'>
+                  <img onClick={() => setCount(count - 1)} className='less' src={less} alt="signe moins" />
+                  <img onClick={() => setCount(count + 1)} className='plus' src={plus} alt="signe plus" />
+                </div>
+              </div>  
+          </div>
+             
+        </div>
+
+
       </div>
 
       <div className='Basket-PriceListe'>
         <div>
         <div className='Basket-trait'>
-          {blocProductPrice}
+          <div className='Basket-ligne'>
+            <div className='Basket-productName'>petit croissant au beurre</div>
+            <div className='Basket-productPrice'>8</div>
+          </div>
         </div>
           <div className='Basket-paye'>
             <NavLink to="/checkout" className='Basket-btnPayer'>PAYER</NavLink>
-            <div className='Basket-totalPrice'>{Totalàpayer}€</div>
+            <div className='Basket-totalPrice'>18€</div>
           </div>
         </div>
       </div>
     </div>
     
-  );
-};
+  );};
+
 
 // == Export
 export default CheckoutBasket;
