9e0d86ee2be726252dc7c4ab20d35dd4da6afe18
karenbomer <karenmaciel@outlook.fr>
2022-04-11 09:56:00 +0200

    plus et moins des produits

--- src/components/BakeryProducts/Product/Product.js
+++ src/components/BakeryProducts/Product/Product.js
@@ -2,12 +2,36 @@ import './styles.scss';
 
 import { NavLink } from "react-router-dom";
 import croissant from './images/Pains-au-chocolat-brioché3.jpg';
+import less from './images/minus.png';
+import plus from './images/plus.png';
 
 
 const Product = () => {
 
+  let blocProduct = [];
+
+  // pour chaque produit (different) on boucle l'etiquette; et les lignes de prix
+  for (let i = 0; i<ProductNumber; i++){
+
+    const [count, setCount] = useState(0);
+    if (count <= -1){
+      count = 0
+    }
+    
+    blocProduct.push(
+      <div className='Basket-product'>
+        <img className='Basket-imgProduit' src={croissant} alt="img-croissant" />
+        <div className='Basket-quantity'>{count}
+            <div className='math'>
+              <img onClick={() => setCount(count - 1)} className='less' src={less} alt="signe moins" />
+              <img onClick={() => setCount(count + 1)} className='plus' src={plus} alt="signe plus" />
+            </div>
+        </div>     
+      </div>
+    )};
 
   return (
+
     <li className="product">
       <img className="product_img" src={croissant} alt="croissant" />
       <div className='product-info'>
