15bdb49992b324a08a3cffe2c80b5eec6c300f33
karenbomer <karenmaciel@outlook.fr>
2022-04-12 15:06:40 +0200

    page produit css responsive ok

--- src/components/BakeryProducts/index.js
+++ src/components/BakeryProducts/index.js
@@ -5,6 +5,7 @@ import StarsRating from 'react-stars-rating/build/components/StarsRating';
 import Product from './Product/Product';
 import Heart from 'src/components/Heart/Heart';
 import croissant from './images/croissant.jpg';
+import { BiSearch } from "react-icons/bi";
 
 const BakeryProducts = () => {
 
@@ -15,20 +16,25 @@ const BakeryProducts = () => {
 
       <div className='bakery-bakery'>
         <div className='bakery-infos'>
-          <div>
+          
             <img className="bakery-img" src={croissant} alt="croissant" />
+            <div className='bakery-right-side'>
             <h2 className="bakery-name">O'Lyon</h2>
-            <StarsRating />
-          </div>
-          <input type='text' />
-          <div className='bakery__like'>
-            <Heart />
-          </div>
+            <StarsRating className='bakery-rating' />
+
+            <div>
+            <Heart className='bakery-like'/>
+            </div>
+            </div>
+             
         </div>
       </div>
-     
+
       <div className='bakery-all-products'>
+        <div className='bakery-search'>
       <h2 className="bakery-products">Nos produits</h2>
+      <BiSearch className="bakery-location"/>
+      </div>
         <ul className='bakery-products-list'>
           <Product />
           <Product />
@@ -43,7 +49,7 @@ const BakeryProducts = () => {
           <Product />
           <Product />
         </ul>
-        <div>
+        <div className='basket-access' >
           <NavLink to="/basket" className='bakery-btn-to-basket'>Accéder au panier</NavLink>
         </div>
       </div>
