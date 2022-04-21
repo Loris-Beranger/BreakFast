71fe37392442729e4d7f0521e63af452beae5ea5
karenbomer <karenmaciel@outlook.fr>
2022-04-21 15:04:32 +0200

    try to create a carousel for the homepage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -2,11 +2,12 @@
 import FormAdress from './FormAdress';
 import './styles.scss';
 import Categories from './Categories';
-
+import Carrousel from 'src/components/Carrousel';
 
 // == Composant
 const HomePage = () => (
   <div className="homepage">
+    <Carrousel />
     <div className="section-form-adress">
       <FormAdress />
     </div>
