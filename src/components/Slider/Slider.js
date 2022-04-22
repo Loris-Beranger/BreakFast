fb9fb2a31db4ac8fcff4b576d2c9cfecfa2010c0
karenbomer <karenmaciel@outlook.fr>
2022-04-22 14:25:34 +0200

    test

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,7 +3,7 @@ import './styles.scss'
 import BtnSlider from './BtnSlider'
 import dataSlider from './dataSlider'
 
-export default function Slider() {
+export default function Slider() { test
 
     const [slideIndex, setSlideIndex] = useState(1)
 
